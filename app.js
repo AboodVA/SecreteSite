53c0ff88e3c5e5b51d3de3f8889b238b49003d6a
Abdullah <Abdullah Al-araj@hotmail.com>
2022-03-26 18:01:23 +0300

    Add enviromental varibales

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 //jshint esversion:6
+require("dotenv").config();
 const express = require ("express");
 const bodyParpser = require("body-parser");
 const ejs = require("ejs");
@@ -6,6 +7,7 @@ const mongoose = require("mongoose");
 const encrypt = require("mongoose-encryption");
 
 const app = express();
+
 
 app.use(express.static("public"));
 app.use(bodyParpser.urlencoded({extended: true}));
@@ -18,7 +20,8 @@ const userSchema =new mongoose.Schema ({
   password: String
 });
 
-const secret = "Thisisourlittlesecret."
+const secret = process.env.SECRET;
+
 userSchema.plugin(encrypt, {secret: secret, encryptedFields: ["password"]});
 
 const User = new mongoose.model("User", userSchema);
